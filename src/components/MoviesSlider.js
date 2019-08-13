c0b60726356cfdb1773e7955285ffa624dded495
john2796 <jbmiranda22796@gmail.com>
2019-08-13 13:00:49 -0700

    need to add loading

--- src/components/MoviesSlider.js
+++ src/components/MoviesSlider.js
@@ -2,11 +2,12 @@ import React, { useEffect, memo } from 'react'
 import PropTypes from 'prop-types'
 
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Slider from 'react-slick'
 
 function MoviesSlider({ state, action, name = '/movie' }) {
+  const { isLoading } = useSelector(state => state.movieReducer)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(action())
@@ -19,6 +20,35 @@ function MoviesSlider({ state, action, name = '/movie' }) {
     slidesToShow: 6,
     slidesToScroll: 5,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
+  if (isLoading) {
+    return <h2 style={{ minHeight: '300px' }}>Loading...</h2>
   }
   return (
     <Slider {...settings}>
@@ -27,7 +57,11 @@ function MoviesSlider({ state, action, name = '/movie' }) {
         return (
           <div key={item.id} id="slider-image-wrap">
             <Link to={`${name}/${item.id}`}>
-              <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt="poster_path" />
+              <img
+                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                alt="poster_path"
+                style={{ minHeight: '300px' }}
+              />
             </Link>
           </div>
         )
