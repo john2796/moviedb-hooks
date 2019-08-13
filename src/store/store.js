e90a6402bb1dc7a49843368933b6192b9040c9cc
john2796 <jbmiranda22796@gmail.com>
2019-08-12 19:09:49 -0700

    removed middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+// import logger from 'redux-logger'
+import rootReducer from './reducers'
 
-const middleware = [thunk, logger];
-const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
-export default store;
+const middleware = [thunk]
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)))
+export default store
