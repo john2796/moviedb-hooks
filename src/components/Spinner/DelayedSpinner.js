685d3b9c9ebb5ae99ec3cc43272d3056127823e7
john2796 <jbmiranda22796@gmail.com>
2019-08-16 21:49:16 -0700

    fixed DelayedSpinner

--- src/components/Spinner/DelayedSpinner.js
+++ src/components/Spinner/DelayedSpinner.js
@@ -6,12 +6,13 @@ class DelayedSpinner extends Component {
     showSpinner: false,
   }
 
-  // when the page load if after 750 ms set spinner to true
+  // switch showSpinner to true on page mount after 750ms
   componentDidMount() {
     const { delay } = this.props
     this.timer = setTimeout(() => this.setState({ showSpinner: true }), delay)
   }
 
+  // prevent memory leak
   componentWillUnmount() {
     clearTimeout(this.timer)
   }
