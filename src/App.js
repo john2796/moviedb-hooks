3676a8fcfb131ca65b87c9007a2ea09c9b87e1da
john2796 <jbmiranda22796@gmail.com>
2019-08-21 22:22:11 -0700

    adding grid layout for spa movies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from './components/Home'
+import SinglePage from './components/single-page/SinglePage'
 
 export default function App() {
   return (
     <>
-      <Switch>
+      {/* <Switch>
         <Route path="/" component={Home} />
-      </Switch>
+      </Switch> */}
+      <SinglePage />
     </>
   )
 }
---
+++ src/components/single-page/SinglePage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './singlePage.scss'
+
+const SinglePage = () => {
+  return (
+    <section className="sp-grid">
+      <div>
+        <nav>
+          <a href="/">logo</a>
+          <a href="/">Music</a>
+          <a href="/">Find</a>
+          <a href="/">Singer</a>
+          <a href="/">Colection</a>
+        </nav>
+      </div>
+      <div>search</div>
+      <div>acount settings</div>
+      <div>banner</div>
+      <div>weekly list</div>
+      <div>Recomender Album</div>
+      <div>Sales Support</div>
+      <div>Recomended artist</div>
+    </section>
+  )
+}
+
+export default SinglePage
